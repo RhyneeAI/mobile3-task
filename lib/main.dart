25fc70edf182cd2365cfbf7272c897d778a75405
Luhung Lugina <luhung.tea@gmail.com>
2025-10-25 13:01:19 +0700

    order for running

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-// import 'package:mobile3_task/profile.dart';
-// import 'package:mobile3_task/calculator.dart';
+import 'package:mobile3_task/profile.dart';
+import 'package:mobile3_task/calculator.dart';
 import 'package:mobile3_task/tugas4/news.dart';
 
+
 void main() {
-  runApp(const NewsApp());
+  runApp(const NewsApp()); // Tugas 4 (uncomment for viewing)
+  // runApp(const Calculator()); // Tugas 3 (uncomment for viewing)
+  // runApp(const ProfileCard()); // Tugas 2 (uncomment for viewing)
 }
